5847de0d7a1ca430e91b9f63889951102f53c403
inforserv <84640913+inforserv@users.noreply.github.com>
2023-11-03 16:43:14 +0000

    First Release

--- app.py
+++ app.py
@@ -13,9 +13,7 @@
 # limitations under the License.
 
 import streamlit as st
-import pandas as pd
-import webbrowser
-from datetime import datetime
+from st_pages import Page, show_pages
 
 st.set_page_config(
     page_title="FIFA23 OFFICIAL DATASET! ⚽️",
@@ -29,37 +27,12 @@ st.set_page_config(
     }
 )
 
-if "data" not in st.session_state:
-    df_data = pd.read_csv("CLEAN_FIFA23_official_data.csv", index_col=0)
-    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
-    df_data = df_data[df_data["Value(£)"] > 0]
-    df_data = df_data.sort_values(by="Value(£)", ascending=False)
-    st.session_state["data"] = df_data
-else:
-    df_data = st.session_state["data"]
+st.write(Page("home.py", "FIFA23 Dataset", "⚽️"))
 
-st.sidebar.markdown("""---""")
-st.sidebar.markdown("Desenvolvido por [InforServ](https://www.inforserv.pt)")
-    
-st.write("# FIFA23 OFFICIAL DATASET! ⚽️")
-
-btn = st.button("Acesse os dados no Kaggle")
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data/data")
-    
-st.markdown(
-    """
-    O conjunto de dados 
-    de jogadores de futebol de 2017 a 2023 fornece informações 
-    abrangentes sobre jogadores de futebol profissionais. 
-    O conjunto de dados contém uma ampla gama de atributos, incluindo dados 
-    demográficos dos jogadores, características físicas, estatísticas de jogo, 
-    detalhes de contratos e afiliações de clubes. 
-    
-    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
-    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
-    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
-    desempenho, avaliação de mercado, análise de clubes, posicionamento de 
-    jogadores e desenvolvimento do jogador ao longo do tempo.
-    """
-    )        
+show_pages(
+    [
+        Page("home.py", "FIFA23 Dataset", "⚽️"),
+        Page("pages/0_players.py", "Jogadores", "👨"),
+        Page("pages/1_teams.py", "Equipas", "❎"),
+    ]
+)
